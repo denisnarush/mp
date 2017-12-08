47cd8ca00d638b097f9895490e328a785a4a436d
denisnarush <child.denis@gmail.com>
2017-12-08 12:17:02 +0300

    search refresh button temporarily updates the playlist

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -10,6 +10,8 @@
     const actionNext = document.getElementById("actionNext");
     const actionPrev = document.getElementById("actionPrev");
 
+    const searchRefresh = document.getElementById("searchRefresh");
+
     const streamCurrentTime = document.getElementById("streamCurrentTime");
     const streamBgArtwork = document.getElementById("streamBgArtwork");
     const streamTrackbar = document.getElementById("streamTrackbar");
@@ -29,7 +31,6 @@
         REDIRECT_URI = "http://www.player-denisnarush.c9.io",
 
         // soundcloud search params
-        OFFSET = Math.floor(Math.random() * (5000 - 0)) + 0,
         LIMIT = 30,
         GENRE = "Chillout";
 
@@ -41,7 +42,7 @@
         redirect_uri: REDIRECT_URI
     });
 
-
+    streamBgArtwork.style.backgroundImage = "url('"+artworkUrl+"')";
 
     stream.addEventListener("canplaythrough", function () {
         stream.play();
@@ -75,71 +76,70 @@
         streamCurrentTime.innerHTML = (stream.currentTime / 60).toString().split(".")[0] + "."+ s;
     });
 
-    streamBgArtwork.style.backgroundImage = "url('"+artworkUrl+"')";
+    searchRefresh.addEventListener("click", function () {
+        getTracks();
+    });
+    
+    getTracks();
 
     function getTrack (trackID) {
         SC.get("/tracks/" + trackID).then(function(data){
-
+    
             stream.pause();
             // set src url to soundcloud stream
             stream.src = data.stream_url + "?client_id=" + CLIENT_ID;
-
+    
             if (data.artwork_url !== null) {
                 artworkUrl = data.artwork_url.replace(new RegExp("large","g"),"t500x500");
             } else {
                 artworkUrl = "https://s-media-cache-ak0.pinimg.com/736x/18/82/0d/18820da64d8732ca79f9161157549b0b.jpg";
             }
-
+    
             streamArtwork.src = artworkUrl;
             streamBgArtwork.style.backgroundImage = "url('"+artworkUrl+"')";
             streamGenre.innerHTML = data.genre;
             streamTitle.innerHTML = data.title;
-
-            // stream.play();
+    
         });
     }
 
-
-    window.getTrack = getTrack; // REMOVE!!!!
-
-
-
-    // search query
-    SC.get("/tracks", {limit: LIMIT, tags: GENRE, offset: OFFSET, client_id: CLIENT_ID,}).then(function(tracks) {
-
-        var html = "";
-
-
-        playlist.tracks = tracks;
-        playlist.current = 0;
-
-        // preload first track
-        getTrack(tracks[0].id);
-
-
-
-        // generate playlist
-        tracks.forEach(function (itm) {
-            var time = new Date();
-            time.setTime(itm.duration);
-            html += "";
-            html += "<div class=\"playlist-item\"  data-trackid=\""+itm.id+"\" onclick=\"getTrack("+itm.id+")\" ontouchstart=\"getTrack("+itm.id+")\">";
-            html += "<div class=\"playlist-item-s playlist-item-s__left\">";
-            html += "<p class=\"playlist-item-title\">"+itm.user.username+"<span class=\"playlist-item-author\">"+itm.title+"</span></p>";
-            html += "</div>";
-            html += "<div class=\"playlist-item-s playlist-item-s__right\">";
-            html += "<p class=\"playlist-item-time\">"+time.getMinutes()+"."+(time.getSeconds() < 10 ? "0"+time.getSeconds() : time.getSeconds())+"</p>";
-            html += "</div>";
-            html += "</div>";
+    function getTracks(){
+        let offset = Math.floor(Math.random() * (5000 - 0)) + 0;
+        // search query
+        SC.get("/tracks", {limit: LIMIT, tags: GENRE, offset: offset, client_id: CLIENT_ID,}).then(function(tracks) {
+        
+            var html = "";
+        
+        
+            playlist.tracks = tracks;
+            playlist.current = 0;
+        
+            // preload first track
+            getTrack(tracks[0].id);
+        
+        
+        
+            // generate playlist
+            tracks.forEach(function (itm) {
+                var time = new Date();
+                time.setTime(itm.duration);
+                html += "";
+                html += "<div class=\"playlist-item\"  data-trackid=\""+itm.id+"\" onclick=\"getTrack("+itm.id+")\" ontouchstart=\"getTrack("+itm.id+")\">";
+                html += "<div class=\"playlist-item-s playlist-item-s__left\">";
+                html += "<p class=\"playlist-item-title\">"+itm.user.username+"<span class=\"playlist-item-author\">"+itm.title+"</span></p>";
+                html += "</div>";
+                html += "<div class=\"playlist-item-s playlist-item-s__right\">";
+                html += "<p class=\"playlist-item-time\">"+time.getMinutes()+"."+(time.getSeconds() < 10 ? "0"+time.getSeconds() : time.getSeconds())+"</p>";
+                html += "</div>";
+                html += "</div>";
+            });
+        
+        
+        
+            // past to the DOM
+            playlist.innerHTML = html;
         });
-
-
-
-        // past to the DOM
-        playlist.innerHTML = html;
-    });
-
-
+    }
 
 
     function onPLay(){
