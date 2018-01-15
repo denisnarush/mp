33412a72760fcf3bf1dc1164bc41dad986bd0c49
denisnarush <child.denis@gmail.com>
2018-01-16 00:39:45 +0300

    service-workers

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import "./js/animation.js";
 import "./js/player.js";
 import "./js/equalizer.js";
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+}
