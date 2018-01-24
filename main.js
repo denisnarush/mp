5859f4dc6408ffd937a0794581f64ba262cb841a
denisnarush <child.denis@gmail.com>
2018-01-24 17:00:54 +0300

    bug

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ import "./js/animation.js";
 import "./js/player.js";
 import "./js/equalizer.js";
 
-if ("serviceWorker" in navigator && nw === undefined) {
+
+
+if ("serviceWorker" in navigator && nw) {
     navigator.serviceWorker.register("service-worker.js");
+}
+
+if (true) {
+    import("./js.js").then(function (f) {
+
+    });
 }
