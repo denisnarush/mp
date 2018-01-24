843e0304e9106e5f3f628aeafed3be45faf75f1d
denisnarush <child.denis@gmail.com>
2018-01-24 15:44:15 +0300

    fixes for nwjs

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+/*global nw*/
 import "./js/animation.js";
 import "./js/player.js";
 import "./js/equalizer.js";
 
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && nw === undefined) {
     navigator.serviceWorker.register("service-worker.js");
 }
