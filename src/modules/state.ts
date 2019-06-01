b1efe594043925c9712b59395265a67695f3cd69
Denis Narush <child.deni@gmail.com>
2019-06-02 00:05:59 +0300

    refactoring: adding typescript

---
+++ src/modules/state.ts
@@ -0,0 +1,34 @@
+import { Player } from "./player";
+
+interface StateElements {
+    [key: string]: Element;
+}
+
+interface StateOptions {
+    player?: Player;
+}
+
+export class State {
+    private elements: StateElements = {};
+
+    constructor(name: string, options: StateOptions = {}) {
+        let container = document.querySelector(`[data-state="${name}"]`);
+
+        if (!container) {
+            container = document.createElement("div");
+            container.classList.add("state");
+            container.setAttribute("data-state", name);
+            container.setAttribute("off", "");
+            document.body.appendChild(container);
+        }
+
+        Object.assign(this, options);
+
+        this.elements["container"] = container;
+
+        container.querySelectorAll(`[data-element]`).forEach((el) => {
+            this.elements[el.getAttribute('data-element')] = el;
+        })
+
+    }
+}
